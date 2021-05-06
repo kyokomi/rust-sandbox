68350442e18f94ef3c743e59772755c296b3082f
kyokomi <email@kyokomi.dev>
2021-05-06 15:47:54 +0900

    fix 17

---
+++ blog/src/main.rs
@@ -0,0 +1,22 @@
+extern crate blog;
+
+use blog::{Post, Post2};
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+
+    let mut post = Post2::new();
+    post.add_text("I ate a salad for lunch today");
+    let post = post.request_review();
+    let post = post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+}
